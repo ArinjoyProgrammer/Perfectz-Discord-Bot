b4cf60a263d1ae160a5836ac86883054438df23c
ArinjoyTheDev <90265138+ArinjoyProgrammer@users.noreply.github.com>
2021-12-13 19:32:46 +0530

    Added some more Commands
    Added some more Commands

--- cogs/fun_commands.py
+++ cogs/fun_commands.py
@@ -24,6 +24,33 @@ class FunCommands(commands.Cog):
 
         embed = discord.Embed(title="What is your Cool Rate?", description=f"Your **Cool Rate** is {coolrate}**%\nDon't forget to check your **Cool Rate** again!")
 
+
+    @commands.command()
+    async def hack(self, ctx, member : discord.Member):
+
+        random_password = ['36b29c', 'kt764a', '45cvv76', 'fh90;ll', 'gda889', 'kla789', 'kkk89q', 'ff89f52', '95632lrt']
+
+        await ctx.send(f"Hacking Satrted on {member}")
+        await ctx.send(f"Hacking Username and Password of {member}")
+        await asyncio.sleep(4)
+        await ctx.send(f"Hacked!\nUsername - ``{member}``\nPassword - ``{random.choice(random_password)}``")
+        await asyncio.sleep(4)
+        await ctx.send("Hacking Completed 20%")
+        await asyncio.sleep(3)
+        await ctx.send("Hacking Completed 30%")
+        await asyncio.sleep(3)
+        await ctx.send("Hacking Completed 50%")
+        await asyncio.sleep(3)
+        await ctx.send("Hacking Epic Games and Steam Account")
+        await asyncio.sleep(2)
+        await ctx.send("Hacking Epic Games and Steam Account Completed Successfully")
+        await asyncio.sleep(3)
+        await ctx.send("Hacking Completed 80%")
+        await asyncio.sleep(3)
+        await ctx.send("Hacking Completed 90%")
+        await asyncio.sleep(4)
+        await ctx.send(f"A **Dangerous Hacking** Completed on **{member}**\nDon't mind this was a really **Fake Hack**")
+
         
 
 def setup(client):
--- cogs/special_commands.py
+++ cogs/special_commands.py
@@ -83,5 +83,82 @@ class SpecialCommands(commands.Cog):
         await ctx.send("The maintenance is now going on!")
 
 
+    @commands.command()
+    async def serverinfo(self, ctx):
+        name = str(ctx.guild.name)
+
+        server_name = str(ctx.guild.name)
+        id = str(ctx.guild.id)
+        region = str(ctx.guild.region)
+        memberCount = str(ctx.guild.member_count)
+
+        icon = str(ctx.guild.icon_url)
+
+        embed = discord.Embed(
+            title=name + " Server Information",
+            description="",
+            color=discord.Color.random()
+        )
+        embed.set_thumbnail(url=icon)
+        embed.add_field(name="Server Name", value=server_name, inline=True)
+        embed.add_field(name="Server ID", value=id, inline=True)
+        embed.add_field(name="Channels: ", value=len(
+            ctx.message.guild.channels), inline=True)
+        embed.add_field(name="Country", value=region, inline=True)
+        embed.add_field(name="Member Count", value=memberCount, inline=True)
+        embed.add_field(name="Requested By: ", value=str(
+            ctx.message.author.mention), inline=False)
+
+        await ctx.send(embed=embed)
+
+
+    @commands.command()
+    async def whois(self, ctx, user: discord.Member = None):
+
+        created_at = user.created_at
+        joined_at = user.joined_at
+
+        created = created_at.strftime("Date: %d/%m/%y  |  Time: %H:%M:%S")
+        joined = joined_at.strftime("Date: %d/%m/%y  |  Time: %H:%M:%S")
+
+        if user == None:
+            user = ctx.author
+
+        rlist = []
+        for role in user.roles:
+            if role.name != "@everyone":
+                rlist.append(role.mention)
+
+        b = ", ".join(rlist)
+
+        embed = discord.Embed(colour=user.color, timestamp=datetime.now())
+
+        embed.set_author(name=f"User Info - {user}"),
+        embed.set_thumbnail(url=user.avatar_url),
+
+        embed.add_field(name='ID:', value=user.id, inline=False)
+        embed.add_field(name='Name:', value=user.display_name, inline=False)
+
+        embed.add_field(name='Created at:', value=created, inline=False)
+        embed.add_field(name='Joined at:', value=joined, inline=False)
+
+        embed.add_field(name='Bot?', value=user.bot, inline=False)
+
+        embed.add_field(name=f'Roles:({len(rlist)})',
+                        value=''.join([b]), inline=False)
+        embed.add_field(name='Top Role:',
+                        value=user.top_role.mention, inline=False)
+
+        await ctx.send(embed=embed)
+
+
+    @commands.command()
+    async def joined(self, ctx, *, member: discord.Member):
+        joined_at = member.joined_at
+        time_joined = joined_at.strftime("Date: %d/%m/%y  |  Time: %H:%M:%S")
+
+        await ctx.send(f"**{member}** joined on **{time_joined}**")
+
+
 def setup(client):
     client.add_cog(SpecialCommands(client))
